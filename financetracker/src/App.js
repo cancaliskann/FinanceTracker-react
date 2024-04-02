1c43390a1482f1c1958eec8d92f827eca960d14f
can caliskan <podcastwerv2017@gmail.com>
2024-04-02 11:05:13 +0300

    css and components

--- financetracker/src/App.js
+++ financetracker/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/incomeExpenses';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
 
 function App() {
   return (
    <div>
-    Hellow
+    <Header />
+    <div className='container'>
+      <Balance />
+      <IncomeExpenses/>
+      <TransactionList/>
+      <AddTransaction/>
+    </div>
    </div>
   );
 }

aba5c5514073e1cbe02d0107b0ceb38b0e6dd576
can caliskan <podcastwerv2017@gmail.com>
2024-04-03 11:11:02 +0300

    income expense

--- financetracker/src/App.js
+++ financetracker/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Header from './components/Header';
 import Balance from './components/Balance';
-import IncomeExpenses from './components/incomeExpenses';
+import { IncomeExpenses } from './components/IncomeExpenses';
+
 import TransactionList from './components/TransactionList';
 import AddTransaction from './components/AddTransaction';
 

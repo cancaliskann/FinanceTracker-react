2235f72dc3fdeb5d48aa54dc5758cb55b710ea0c
can caliskan <podcastwerv2017@gmail.com>
2024-04-04 09:13:41 +0300

    final

--- financetracker/src/context/GlobalState.js
+++ financetracker/src/context/GlobalState.js
@@ -3,10 +3,7 @@ import AppReducer from "./AppReducer";
 
 const initialState= {
     transactions: [
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }
+     
     ]
 
     
@@ -14,10 +11,27 @@ const initialState= {
 
 export const GlobalContext = createContext(initialState);
 
-export const GlobalProvider = ({children}) => {
-    const [state,dispatch] = useReducer(AppReducer, initialState);
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+  
+    function deleteTransaction(id) {
+      dispatch({
+        type: 'DELETE_TRANSACTION',
+        payload: id
+      });
+    }
 
-    return (<GlobalContext.Provider value={{transactions:state.transactions}}>
+    function addTransaction(transaction) {
+        dispatch({
+          type: 'ADD_TRANSACTION',
+          payload: transaction
+        });
+      }
+
+    return (<GlobalContext.Provider value={{transactions:state.transactions,
+    deleteTransaction,
+    addTransaction
+    }}>
         {children}
     </GlobalContext.Provider>);
 
